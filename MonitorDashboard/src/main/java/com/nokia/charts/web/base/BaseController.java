435a4fb1f106776c5355e1bb7a02b6dd5c8803cc
Ma <lian.ma@capgemini.com>
2017-09-23 23:28:45 +0800

    test

--- MonitorDashboard/src/main/java/com/nokia/charts/web/base/BaseController.java
+++
@@ -1,8 +0,0 @@
-package com.nokia.charts.web.base;
-
-import org.apache.log4j.Logger;
-
-public class BaseController {
-
-	public static final Logger logger = Logger.getLogger(BaseController.class);
-}
